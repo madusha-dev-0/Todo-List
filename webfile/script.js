2962454dc5a737b2170e5f5da25de4d612da8d81
madusha-dev-0 <kumaramadushan216@gmail.com>
2026-06-24 18:22:05 +0530

    Add Delete button , check button modify

--- webfile/script.js
+++ webfile/script.js
@@ -5,24 +5,46 @@ const taskContainer = document.getElementById("task-container");
 
 /* ---------------------add task------------------------ */
 
-addButton.addEventListener("click", function(event){
+addButton.addEventListener("click", function (event) {
 
-    let taskAdd = document.createElement("input");
-    taskAdd.setAttribute("type" ,"checkbox" );
-    taskAdd.setAttribute("class", "check");
+    let checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("class", "check");
 
     let taskDesc = document.createElement("p");
     taskDesc.innerHTML = inputTask.value;
 
+    let deleteIcon = document.createElement("button");
+    deleteIcon.setAttribute("id", "delete-icon");
+
+
+
     let divContainer = document.createElement("div");
+    divContainer.setAttribute("id", "div-container");
 
-    divContainer.setAttribute("id","divcontainer")
 
-    divContainer.appendChild(taskAdd);
+    divContainer.appendChild(checkBox);
     divContainer.appendChild(taskDesc);
+    divContainer.appendChild(deleteIcon);
 
     taskContainer.appendChild(divContainer).innerHTML;
 
+    checkBox.addEventListener("change",function(event){
+        if(checkBox.checked){
+            taskDesc.style.textDecoration = "line-through";
+            taskDesc.style.textDecorationColor = "red";
+            taskDesc.style.textDecorationThickness = "2px"
+            taskDesc.style.textDecorationStyle = "double";
+            
+        }
+    })
+
+
+
+    deleteIcon.addEventListener("click", function (event) {
+        taskContainer.removeChild(divContainer);
+    })
 
 
 })
+
